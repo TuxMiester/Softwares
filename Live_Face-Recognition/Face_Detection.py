afdac074f8ce9ad5e7dda3c9021f2fe362c29e9c
tuxx <starknet87@gmail.com>
2021-09-15 01:13:56 +0530

    MachineReady

---
+++ Live_Face-Recognition/Face_Detection.py
@@ -0,0 +1,28 @@
+import cv2
+from random import randrange
+trained_face_data = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+
+#img = cv2.imread('Images/MomAishCW.jpg')
+webcam = cv2.VideoCapture(0)
+
+while True:
+    successful_frame_read, frame = webcam.read()
+    grayscaled_img = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    face_coordinates = trained_face_data.detectMultiScale(grayscaled_img)
+    for (x, y, w, h) in face_coordinates:
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (randrange(256), randrange(256), randrange(256)), 2)
+    cv2.imshow('Face Detector', frame)
+    key = cv2.waitKey(1)
+
+    if key==27:
+        break
+webcam.release()
+
+
+
+#print(face_coordinates)
+
+
+
+
+print("Code Completed")
